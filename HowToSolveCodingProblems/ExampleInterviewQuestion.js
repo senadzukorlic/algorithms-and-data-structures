12978984ce91fd4799c3c3cb0dd14ce4d5a40a2f
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-11-28 17:31:08 +0100

    excersice - interview question

--- HowToSolveCodingProblems/ExampleInterviewQuestion.js
+++ HowToSolveCodingProblems/ExampleInterviewQuestion.js
@@ -4,36 +4,48 @@
 //const array2 = ['z','y','i']
 //should return false.
 //------------
-//const array1 = ['a','b','c','x']
-//const array2 = ['z','y','x']
+// const array1 = ["a", "b", "c", "x"]
+// const array2 = ["z", "y", "x"]
 //should return true.
 
-function findCommonCharacter(array1, array2) {
-  let lgt
+// function findCommonCharacter(array1, array2) {
+//   for (let i = 0; i < array1.length; i++) {
+//     for (let j = 0; j < array1.length; j++) {
+//       if (array1[i] === array2[j]) {
+//         return true
+//       }
+//     }
+//   }
+// }
 
-  if (array1.length > array2.length) {
-    lgt = array1.length
-  } else {
-    lgt = array2.length
-  }
-
-  for (let i = 0; i < lgt; i++) {
-    for (let j = 0; j < lgt; j++) {
-      if (array1[i] === array2[j]) {
-        console.log(
-          `Element niza 1 pod rednim brojem - [${[
-            i,
-          ]}], je jednak sa elementom niza 2 pod rednim brojem - [${[j]}]`
-        )
-      }
+function containsCommonItem2(arr1, arr2) {
+  // loop through first array and create object
+  // where properties === items in the array
+  let map = {}
+  for (let i = 0; i < arr1.length; i++) {
+    if (!map[arr1[i]]) {
+      const item = arr1[i]
+      map[item] = true
     }
   }
-}
-const arr1 = ["z", "y", "x"]
-const arr2 = ["a", "b", "c", "x"]
-findCommonCharacter(arr1, arr2)
 
-function findCommonCharacter2(array1, array2) {
-  // loop through first array and create object where properties === items in the array
-  // loop through second array and check if item in second array exists on created object.
+  // loop through second array and check if
+  // item in second array exists on created
+  // object.
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (map[arr2[j]]) {
+      return true
+    }
+  }
+  return false
 }
+const array1 = ["a", "b", "c", "x"]
+const array2 = ["z", "y", "x"]
+console.log(containsCommonItem2(array1, array2))
+
+function containsCommonItem3(arr1, arr2) {
+  return arr1.some((item) => arr2.includes(item))
+}
+
+console.log(containsCommonItem3(array1, array2))
